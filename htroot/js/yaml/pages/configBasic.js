a8f0750955f0f1b5c845fbce1693670302f4e40a
Jens Bertram <code@jens-bertram.net>
2013-12-05 12:23:57 +0100

    add validation code

---
+++ htroot/js/yaml/pages/configBasic.js
@@ -0,0 +1,47 @@
+/** Initialize the page. */
+YaCyPage.init = function() {
+  var validator = new YaCyUi.Form.Validator({
+    toggle:$('#submitConfig'),
+    display:$('#ycu-error-count')
+  });
+
+  validator
+  .addElement($('#peerName'), {
+    onload: true,
+    validators: [{
+      type: 'notEmpty',
+      error: 'empty'
+    },
+    {
+      type: 'regEx',
+      exp: /^[a-zA-Z0-9\-_]+$/,
+      error: 'characters'
+    },
+    {
+      type: 'length',
+      min: 3,
+      max: 80,
+      error: 'length'
+    }],
+  })
+  .addElement($('#peerPort'), {
+    onload:true,
+    validators: [{
+      type: 'notEmpty',
+      error: 'empty'
+    },
+    {
+      type: 'range',
+      min:0,
+      max:65535,
+      nan: 'nan'
+    },
+    {
+      type: 'range',
+      min:0,
+      max:1023,
+      invert:true,
+      failType: 'warning'
+    }]
+  });
+};
