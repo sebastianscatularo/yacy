512196075198c04ef960113e43288b35b8903a55
Jens Bertram <code@jens-bertram.net>
2013-12-07 20:31:53 +0100

    html fixes add validator

---
+++ htroot/js/yaml/pages/proxyIndexingMonitor.js
@@ -0,0 +1,26 @@
+/** Initialize the page validators. */
+YaCyPage.init = function() {
+  // init parts
+  var validator = new YaCyUi.Form.Validator({
+    toggle: $('#submitSave'),
+    display: $('#ycuErrorCount'),
+    onload: true
+  });
+  validator.addElement($('#proxyPrefetchDepth'), {
+    validators: [{
+      type: 'notEmpty'
+    }, {
+      type: 'range',
+      min: $('#proxyPrefetchDepth').data('min'),
+      max: $('#proxyPrefetchDepth').data('max')
+    }]
+  }).addElement($('#HTCache_size'), {
+    validators: [{
+      type: 'notEmpty'
+    }, {
+      type: 'range',
+      min: $('#HTCache_size').data('min'),
+      max: $('#HTCache_size').data('max')
+    }]
+  });
+};
