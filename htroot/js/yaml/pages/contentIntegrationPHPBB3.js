3a4451c0c4b4142f0aaceb3bb3ffe1fb6df2c627
Jens Bertram <code@jens-bertram.net>
2013-12-07 21:13:38 +0100

    fix html add validators

---
+++ htroot/js/yaml/pages/contentIntegrationPHPBB3.js
@@ -0,0 +1,51 @@
+/** Initialize the page validators. */
+YaCyPage.init = function() {
+  // init parts
+  var validatorExport = new YaCyUi.Form.Validator({
+    toggle: $('#submitCheck, #submitExport'),
+    display: $('#ycuErrorCountExport'),
+    onload: true
+  });
+  validatorExport.addElement($('#urlStub'), {
+    validators: [{
+      type: 'notEmpty',
+      error: 'empty'
+    }, {
+      type: 'url',
+      error: 'invalid'
+    }, {
+      type: 'urlProtocol',
+      protocols: ['https?'],
+      error: 'protocol'
+    }]
+  }).addElement($('#postsPerFile'), {
+    validators: [{
+      type: 'range',
+      min: $('#postsPerFile').data('min')
+    }]
+  }).addElement($('#dbHost'), {
+    validators: [{
+      type: 'notEmpty'
+    }]
+  }).addElement($('#dbPort'), {
+    validators: [{
+      type: 'number'
+    }]
+  }).addElement($('#dbName, #dbUser, #dbPw'), {
+    validators: [{
+      type: 'notEmpty'
+    }]
+  });
+
+  var validatorImport = new YaCyUi.Form.Validator({
+    toggle: $('#submitImport'),
+    display: $('#ycuErrorCountImport'),
+    onload: true,
+    showLink: false
+  });
+  validatorImport.addElement($('#dumpFile'), {
+    validators: [{
+      type: 'notEmpty'
+    }]
+  });
+};
