e72e9fdea55974decc99cea4de42ab34449a015a
Jens Bertram <code@jens-bertram.net>
2013-12-08 10:11:57 +0100

    update script to be usable again

--- htroot/js/yaml/pages/crawlStartSite.js
+++ htroot/js/yaml/pages/crawlStartSite.js
@@ -1,92 +1,53 @@
-YaCyPage.Parts = {};
+/** Initialize the page. */
+YaCyPage.init = function() {
+  var validator = new YaCyUi.Form.Validator({
+    toggle: $('#submitCrawl'),
+    display: $('#ycu-error-count'),
+    onload: true
+  }).addElement($('#crawlingURL'), {
+    validators: [{
+      type: 'notEmpty',
+      error: 'empty'
+    }, {
+      type: 'url',
+      error: 'invalid'
+    }, {
+      type: 'urlProtocol',
+      protocols: ['https?', 'file', 'ftp', 'smb'],
+      error: 'protocol'
+    }]
+  });
 
-YaCyPage.Parts.StartPoint = function() {
-  var self = this;
-  var root = $('#startPoint');
-  this.e = {
-    crawlingURL: $('#crawlingURL'),
-    startPointDetails: $('#startPointDetails'),
-    startPointSelect: $('#startPointSelect'),
-    btnBar: root.find('*[data-id="getSiteData"]'),
-    btnRobotsAndStructure: root.find('button[data-id="robotsAndStructure"]'),
-  };
-  var valid = false;
-
-  this.pagesInfoLoaded = function() {
-    // hide buttons..
-    this.e.btnBar.hide();
-    // ..and reset
-    this.e.btnRobotsAndStructure.prop('disabled', false);
-    YaCyUi.Form.Button.switchIcon(this.e.btnRobotsAndStructure);
-    YaCyUi.Form.Button.switchText(this.e.btnRobotsAndStructure);
-  };
-
-  this.addInteractionHandler = function() {
-    // validator
-    YaCyUi.Form.Validate.addValidator(self.e.crawlingURL, {
-      func: new YaCyUi.Form.Validate.Validators.url().validate,
-      delay: YaCyPage.validationDelay,
-      onload: true
-    });
-
-    // crawl start point button
-    self.e.btnRobotsAndStructure.prop('disabled', false).click(function(evObj) {
+  $('#startPoint').find('button[data-id="robotsAndStructure"]').on('click',
+    function(evObj) {
       evObj.preventDefault();
       $(this).prop('disabled', true);
       YaCyUi.Form.Button.switchIcon($(this), 'icon-loader');
       YaCyUi.Form.Button.switchText($(this), 'Loading..');
-      YaCyPage.CrawlStart.getPagesInfo(self.pagesInfoLoaded, self);
+      YaCyPage.CrawlStart.getPagesInfo(function() {
+        var btn = $('#startPoint').find('button[data-id="robotsAndStructure"]');
+        // hide buttons..pagesInfoLoaded
+        $('#startPoint').find('*[data-id="getSiteData"]').hide();
+        // ..and reset
+        btn.prop('disabled', false);
+        YaCyUi.Form.Button.switchIcon(btn);
+        YaCyUi.Form.Button.switchText(btn);
+      });
     });
-  };
-
-  this.setValid = function(state) {
-    valid = state;
-  }
-
-  this.isValid = function() {
-    return state;
-  }
-
-  this.addInteractionHandler();
-  return this;
-};
-
-YaCyPage.Parts.FormControl = function() {
-  var self = this;
-  this.errorCount = 0;
-  this.e = {
-    btnSubmit: $('fieldset.formControl').find('button[type="submit"]')
-  };
-};
-
-YaCyPage.EventHandler = function() {
-  var self = this;
-  this.e = {
-    btnBar: $('#startPoint').find('*[data-id="getSiteData"]')
-  };
-
-  this.handle = function(evObj, type, elements) {
-    if (type !== 'valid') {
-      self.e.btnBar.hide();
-    } else {
-      self.e.btnBar.show();
-    }
-  };
-};
-
-/** Initialize the page. */
-YaCyPage.init = function() {
-  new YaCyPage.Parts.FormControl();
-  YaCyPage.CrawlStart = new YaCyPage.Func.CrawlStart();
-
-  // init parts and store validable ones
-  YaCyPage.parts = {
-    startPoint: new YaCyPage.Parts.StartPoint()
-  }
-
-  YaCyPage.eventHandler = new YaCyPage.EventHandler();
 
   YaCyUi.Event.handle('validation-state', function(evObj, type, elements) {
-    YaCyPage.eventHandler.handle(evObj, type, elements);
+    if (elements[0].id == 'crawlingURL') {
+      $('#startPointDetails').hide('slow');
+      if (type == 'valid') {
+        $('#startPoint')
+          .find('button[data-id="robotsAndStructure"]').show();
+        $('#startPoint').find('*[data-id="getSiteData"]').show();
+      } else {
+        $('#startPointSelect').hide();
+      }
+    }
   });
+
+  //new YaCyPage.Parts.FormControl();
+  YaCyPage.CrawlStart = new YaCyPage.Func.CrawlStart();
 };
