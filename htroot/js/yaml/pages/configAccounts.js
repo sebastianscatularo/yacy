abd06bc58cbbdde6f4ca2e9f49dd575acad2b273
Jens Bertram <code@jens-bertram.net>
2013-12-10 12:45:47 +0100

    updated validation

--- htroot/js/yaml/pages/configAccounts.js
+++ htroot/js/yaml/pages/configAccounts.js
@@ -3,14 +3,22 @@
 "use strict";
 /** Initialize the page. */
 YaCyPage.init = function() {
-  var validator = new YaCyUi.Form.Validator({
+  var validatorAdmin = new YaCyUi.Form.Validator({
     toggle: $('#submitAccount'),
-    display: $('#ycu-error-count'),
+    display: $('#ycu-error-count-admin'),
+    showLink: false
   }).addElement($('#adminUser, #adminPw1, #adminPw2'), {
     onload: true,
     validators: [{
       type: 'notEmpty',
       error: 'empty'
+    }]
+  }).addElement($('#adminUser'), {
+    onload: true,
+    validators: [{
+      type: 'length',
+      min: 4,
+      error: 'length'
     }]
   }).addElement($('#adminPw2'), {
     onload: false,
@@ -23,5 +31,29 @@ YaCyPage.init = function() {
 
   $('#adminUser, #adminPw1, #adminPw2').on('focus', function() {
     $('#access_account').prop('checked', true);
+  });
+
+  var validatorUser = new YaCyUi.Form.Validator({
+    toggle: $('#submitUser'),
+    display: $('#ycu-error-count-user'),
+    onload: true
+  }).addElement($('#userName, #password, #password2'), {
+    validators: [{
+      type: 'notEmpty',
+      error: 'empty'
+    }]
+  }).addElement($('#userName'), {
+    onload: false,
+    validators: [{
+      type: 'length',
+      min: 4,
+      error: 'length'
+    }]
+  }).addElement($('#password2'), {
+    validators: [{
+      type: 'same',
+      element: '#password',
+      error: 'notSame'
+    }]
   });
 };
