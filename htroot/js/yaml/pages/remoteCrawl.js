79c6357d72873db9df50b978d8030290298c94b8
Jens Bertram <code@jens-bertram.net>
2013-12-12 13:23:02 +0100

    remove leftover comment

--- htroot/js/yaml/pages/remoteCrawl.js
+++ htroot/js/yaml/pages/remoteCrawl.js
@@ -3,7 +3,6 @@
 "use strict";
 /** Initialize the page validators. */
 YaCyPage.init = function() {
-  // init parts
   var validator = new YaCyUi.Form.Validator({
     toggle: $('#submitSave'),
     display: $('#ycuErrorCount'),
